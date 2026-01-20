b1b242aaa71009f152024953b236975dcd41d878
GabrielP295 <gabyboyxp@gmail.com>
2026-01-20 10:39:46 -0600

    takes in user input for calculating payment from hours worked

--- MyFirstProject/MyFirstProject.cpp
+++ MyFirstProject/MyFirstProject.cpp
@@ -3,12 +3,15 @@ using namespace std;
 
 int main()
 {
-    int hours = 40;
-    double rate = 52;
-	double payment = hours * rate;
+    int hours; 
+    double rate;
+	cout << "Enter number of hours worked: ";
+    cin >> hours;
+    cout << "Enter the pay rate as a decimal: "
+    cin >> rate;
+    double payment = hours * rate;
     double paymentPerMonth = payment * 4;
-	double paymentAcrossInternship = paymentPerMonth * 3;
-    
+    double paymentAcrossInternship = paymentPerMonth * 3 * 0.8;
 
     cout << "Payment is: $" << payment << endl;
 	cout << "Payment per month is: $" << paymentPerMonth << endl;
