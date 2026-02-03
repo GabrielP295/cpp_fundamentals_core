f2c88488f94973e6ddd1ebce5e48519a3c95ad55
GabrielP295 <gabyboyxp@gmail.com>
2026-02-02 19:13:55 -0600

    refractored SortSample file to use functions for finding lowest and highest numbers

--- MyFirstProject/SortSample.cpp
+++ MyFirstProject/SortSample.cpp
@@ -1,6 +1,9 @@
 #include <iostream>;
 using namespace std;
 
+int find_smallest_number(int a, int b, int c, int d, int e);
+int find_largest_number(int a, int b, int c, int d, int e);
+
 int main() {
 	int a, b, c, d, e;
 	cout << "Enter the first integer: ";
@@ -13,7 +16,14 @@ int main() {
 	cin >> d;
 	cout << "Enter the fifth integer: ";
 	cin >> e;
-	int high = a;
+	int low = find_smallest_number(a, b, c, d, e);
+	int high = find_largest_number(a, b, c, d, e);
+
+	cout << "The highest number is ==> " << high << endl;
+	cout << "The lowest number is ==> " << low << endl;
+}
+
+int find_smallest_number(int a, int b, int c, int d, int e) {
 	int low = a;
 	if (a < low) {
 		low = a;
@@ -30,6 +40,10 @@ int main() {
 	if (e < low) {
 		low = e;
 	}
+	return low;
+}
+int find_largest_number(int a, int b, int c, int d, int e) {
+	int high = a;
 	if (a > high) {
 		high = a;
 	}
@@ -45,7 +59,5 @@ int main() {
 	if (e > high) {
 		high = e;
 	}
-	cout << "The highest number is ==> " << high << endl;
-	cout << "The lowest number is ==> " << low << endl;
-
+	return high;
 }
