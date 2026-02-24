dae9a4498f463daf235e586118a3f02ca6d95023
GabrielP295 <gabyboyxp@gmail.com>
2026-02-24 09:57:08 -0600

    Sales Analysis accumalating total monthly sales program completed

---
+++ cpp/projects/SalesAnalysis.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string store;
+	int numOfMonths;
+	double monthlySales;
+	double totalSales = 0;
+
+	cout << "Best Buy Sales Analysis\n";
+	cout << "========================\n";
+	cout << "Enter the store name: ";
+	cin >> store;
+	cout << "How many months do you want to evaluate? ";
+	cin >> numOfMonths;
+
+	for (int i = 1; i <= numOfMonths; i++) {
+		cout << "Enter Sales Month # " << i << ": ";
+		cin >> monthlySales;
+		totalSales += monthlySales;
+	}
+	
+	cout << "Total Sales: " << totalSales;
+}
