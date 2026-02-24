28fc30f41f9c294967fde62a49074862df8e61fe
GabrielP295 <gabyboyxp@gmail.com>
2026-02-24 10:37:43 -0600

    Implemented complete class for SalesAnalysis

--- cpp/projects/SalesAnalysis.cpp
+++ cpp/projects/SalesAnalysis.cpp
@@ -1,3 +1,21 @@
-class SalesAnalysis {
+#include "SalesAnalysis.h"
 
-};
+SalesAnalysis::SalesAnalysis(string store) {
+	this->store = store;
+}
+
+void SalesAnalysis::AcumSales(double sales) {
+	this->totalSales += sales;
+}
+
+double SalesAnalysis::getTotalSales() {
+	return this->totalSales;
+}
+
+double SalesAnalysis::getAverage(int numOfMonths) {
+	return this->totalSales / numOfMonths;
+}
+
+string SalesAnalysis::getStore() {
+	return this->store;
+}
