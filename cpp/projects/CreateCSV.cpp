e452b2bb6840269e430bedaa070054ffdc54f211
Max Jock3 <maksymnapora2106@gmail.com>
2026-04-23 10:36:13 -0500

    feat: partial implementation of CreateCSV application

---
+++ cpp/projects/CreateCSV.cpp
@@ -0,0 +1,15 @@
+#include "FileCreator.h"
+
+int main() {
+	string fileName;
+	getline(cin, fileName);
+
+	FileCreator storeSales = FileCreator(fileName);
+	storeSales.WriteHeader();
+	for (int i = 1; i <= 5; i++) {
+		string storeName;
+		cout << "Enter store name " + i;
+		cin >> storeName;
+	}
+	storeSales.WriteFile();
+}
